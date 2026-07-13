687a9769ec40fffa23561cfc6f3dde622805010b
awaszeciak <a.waszeciak@gmail.com>
2026-07-13 23:46:33 +0200

    Add custom validation messages

--- src/main/java/com/intern/employeeservice/dto/EmployeeCreateRequest.java
+++ src/main/java/com/intern/employeeservice/dto/EmployeeCreateRequest.java
@@ -6,23 +6,24 @@ import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 
 public record EmployeeCreateRequest (
-        @NotBlank
-        @Size(max = 100)
+        @NotBlank(message = "First name must not be blank")
+        @Size(max = 100, message = "First name must not exceed 100 characters")
         String firstName,
 
-        @NotBlank
-        @Size(max = 100)
+        @NotBlank(message = "Last name must not be blank")
+        @Size(max = 100, message = "Last name must not exceed 100 characters")
         String lastName,
 
-        @NotNull
-        @Past
+        @NotNull(message = "Date of birth is required")
+        @Past(message = "Date of birth must be in the past")
         LocalDate dateOfBirth,
 
-        @NotNull
+        @NotNull(message = "Gender is required")
         Gender gender,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{9}")
+        @NotBlank(message = "Social security number must not be blank")
+        @Pattern(regexp = "\\d{9}",
+                message = "Social security number must contain 9 digits")
         String socialSecurityNumber
 ) {
 
--- src/main/java/com/intern/employeeservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/intern/employeeservice/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.intern.employeeservice.exception;
 
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
