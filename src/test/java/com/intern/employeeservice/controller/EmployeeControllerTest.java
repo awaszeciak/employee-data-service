cc6fb33eb7baa515a1fe08b6d709042e477a187e
awaszeciak <a.waszeciak@gmail.com>
2026-07-14 22:46:59 +0200

    Expand employee controller tests

--- src/test/java/com/intern/employeeservice/controller/EmployeeControllerTest.java
+++ src/test/java/com/intern/employeeservice/controller/EmployeeControllerTest.java
@@ -3,6 +3,8 @@ package com.intern.employeeservice.controller;
 import com.intern.employeeservice.dto.EmployeeCreateRequest;
 import com.intern.employeeservice.dto.EmployeeResponse;
 import com.intern.employeeservice.entity.Gender;
+import com.intern.employeeservice.exception.EmployeeAlreadyExistsException;
+import com.intern.employeeservice.exception.EmployeeNotFoundException;
 import com.intern.employeeservice.service.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +14,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -70,4 +75,124 @@ public class EmployeeControllerTest {
 
     }
 
+    @Test
+    void shouldGetEmployeeById() throws Exception {
+
+        EmployeeResponse response = new EmployeeResponse(
+                1L,
+                "Anna",
+                "Nowak",
+                LocalDate.of(1995,4,12),
+                Gender.FEMALE
+        );
+
+        when(employeeService.getEmployeeById(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/employees/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.firstName").value("Anna"))
+                .andExpect(jsonPath("$.lastName").value("Nowak"))
+                .andExpect(jsonPath("$.dateOfBirth").value("1995-04-12"))
+                .andExpect(jsonPath("$.gender").value("FEMALE"))
+                .andExpect(jsonPath("$.socialSecurityNumber").doesNotExist())
+                .andExpect(jsonPath("$.socialSecurityNumberHash").doesNotExist());
+
+        verify(employeeService).getEmployeeById(1L);
+    }
+
+    @Test
+    void shouldGetAllEmployees() throws Exception {
+
+        EmployeeResponse response1 = new EmployeeResponse(
+                1L,
+                "Anna",
+                "Nowak",
+                LocalDate.of(1995,4,12),
+                Gender.FEMALE
+        );
+
+        EmployeeResponse response2 = new EmployeeResponse(
+                2L,
+                "Adam",
+                "Kowalski",
+                LocalDate.of(1983,7,4),
+                Gender.MALE
+        );
+
+        when(employeeService.getAllEmployees()).thenReturn(List.of(response1, response2));
+
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[0].gender").value("FEMALE"))
+                .andExpect(jsonPath("$[1].gender").value("MALE"))
+                .andExpect(jsonPath("$[0].firstName").value("Anna"))
+                .andExpect(jsonPath("$[1].firstName").value("Adam"))
+                .andExpect(jsonPath("$[0].lastName").value("Nowak"))
+                .andExpect(jsonPath("$[1].lastName").value("Kowalski"))
+                .andExpect(jsonPath("$[0].dateOfBirth").value("1995-04-12"))
+                .andExpect(jsonPath("$[1].dateOfBirth").value("1983-07-04"))
+                .andExpect(jsonPath("$[0].socialSecurityNumberHash").doesNotExist())
+                .andExpect(jsonPath("$[1].socialSecurityNumberHash").doesNotExist())
+                .andExpect(jsonPath("$[0].socialSecurityNumber").doesNotExist())
+                .andExpect(jsonPath("$[1].socialSecurityNumber").doesNotExist());
+
+        verify(employeeService).getAllEmployees();
+    }
+
+
+    @Test
+    void shouldReturn404WhenEmployeeNotFound() throws Exception {
+
+        when(employeeService.getEmployeeById(999999L)).thenThrow(new EmployeeNotFoundException("Employee with id 999999 not found"));
+
+        mockMvc.perform(get("/employees/999999"))
+                        .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Employee with id 999999 not found"));
+
+    }
+
+    @Test
+    void shouldReturn409WhenSsnAlreadyExists() throws Exception {
+        EmployeeCreateRequest request = new EmployeeCreateRequest(
+                "Anna",
+                "Nowak",
+                LocalDate.of(1995, 4, 12),
+                Gender.FEMALE,
+                "123456789"
+        );
+
+        when(employeeService.createEmployee(any(EmployeeCreateRequest.class)))
+                .thenThrow(new EmployeeAlreadyExistsException("Employee with this SSN already exists"));
+
+        mockMvc.perform(post("/employees").
+                contentType("application/json")
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(jsonPath("$.message").value("Employee with this SSN already exists"))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void shouldReturn400WhenRequestIsInvalid() throws Exception {
+
+        EmployeeCreateRequest request = new EmployeeCreateRequest(
+                "",
+                "",
+                LocalDate.now().plusDays(1),
+                null,
+                "abc"
+        );
+
+        mockMvc.perform(post("/employees")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Validation failed"))
+                .andExpect(jsonPath("$.details").isArray());
+
+
+    }
 }
